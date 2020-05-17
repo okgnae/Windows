d02e8f3c2fab93681bd124e06581cabb2a39d80f
Reangko <33633448+reangko@users.noreply.github.com>
2020-05-17 15:26:36 +0300

    Update MS-Build-Network.ps1

--- MS-Build-Guide/MS-Build-Network.ps1
+++ MS-Build-Guide/MS-Build-Network.ps1
@@ -1,1 +1,59 @@
+### MS-Build-Network
+### Administrator 12qw!@QW
 
+### Get Management Network Adapter
+$ManagementInterfaceIndex = (Get-NetAdapter).ifIndex[0]
+
+### Define Management Network
+$ManagementIPAddress = "192.168.1.112"
+$ManagementPrefixLength = "24"
+$ManagementServerAddresses = ""
+
+### Add IP, Netmask, Gateway, and DNS to Management NIC
+New-NetIPAddress `
+	-InterfaceIndex $ManagementInterfaceIndex `
+	-IPAddress $ManagementIPAddress `
+	-PrefixLength $ManagementPrefixLength
+
+Set-NetIPInterface -InterfaceIndex $ManagementInterfaceIndex -Dhcp Disabled
+
+Set-DNSClientServerAddress –interfaceIndex $ManagementInterfaceIndex –ServerAddresses $ManagementServerAddresses
+
+### Get Service Network Adapter
+$ServiceInterfaceIndex = (Get-NetAdapter).ifIndex[1]
+
+### Define Service Network
+$ServiceIPAddress = "10.0.0.112"
+$ServicePrefixLength = "24"
+$ServiceDefaultGateway = "10.0.0.1"
+$ServiceServerAddresses = "10.0.0.10"
+
+### Add IP, Netmask, Gateway, and DNS to Service NIC
+New-NetIPAddress `
+	-InterfaceIndex $ServiceInterfaceIndex `
+	-IPAddress $ServiceIPAddress `
+	-PrefixLength $ServicePrefixLength `
+	-DefaultGateway $ServiceDefaultGateway
+
+Set-NetIPInterface -InterfaceIndex $ServiceInterfaceIndex -Dhcp Disabled
+
+Set-DNSClientServerAddress –interfaceIndex $ServiceInterfaceIndex –ServerAddresses $ServiceServerAddresses
+
+# Disable IPv6 on all NICs
+Disable-NetAdapterBinding -Name * -ComponentID ms_tcpip6
+
+### Turn off IPv6 Random & Temporary IP Assignments
+Set-NetIPv6Protocol -RandomizeIdentifiers Disabled
+Set-NetIPv6Protocol -UseTemporaryAddresses Disabled
+
+### Turn off IPv6 Transition Technologies
+Set-Net6to4Configuration -State Disabled
+Set-NetIsatapConfiguration -State Disabled
+Set-NetTeredoConfiguration -Type Disabled
+
+### Define the Computer Name
+$ComputerName = "WINMS1"
+
+### Rename, Reboot
+Rename-Computer -NewName $ComputerName -Force
+Restart-Computer -Force
