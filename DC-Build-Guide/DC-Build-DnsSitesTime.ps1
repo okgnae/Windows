ac1625882ded1f2bb2bf9a9ee837d0599e7a42f7
Reangko <33633448+reangko@users.noreply.github.com>
2020-05-17 12:28:29 +0300

    Create DC-Build-DnsSitesTime.ps1

---
+++ DC-Build-Guide/DC-Build-DnsSitesTime.ps1
@@ -0,0 +1,29 @@
+### DC-Build-DnsSitesTime
+
+### Define DNS and Sites & Services Settings
+$IPv4netID = "10.0.0.0/24"
+$siteName = "HQ"
+$location = "HQ"
+
+### Add DNS Reverse Lookup Zones
+Add-DNSServerPrimaryZone -NetworkID $IPv4netID -ReplicationScope 'Forest' -DynamicUpdate 'Secure' -Verbose
+
+### Make Changes to Sites & Services
+Rename-ADObject (Get-ADReplicationSite).DistinguishedName -NewName $siteName -Verbose
+New-ADReplicationSubnet -Name $IPv4netID -site $siteName -Location $location -Verbose
+Get-ADReplicationSubnet -Filter *
+
+### Re-Register DC's DNS Records
+Register-DnsClient -Verbose
+
+### Enable Default Aging/Scavenging Settings for All Zones and this DNS Server
+Set-DnsServerScavenging –ScavengingState $True –ScavengingInterval 7:00:00:00 –ApplyOnAllZones -Verbose
+Get-DnsServerZone | Where-Object {$_.IsAutoCreated -eq $False -and $_.ZoneName -ne 'TrustAnchors'} | Set-DnsServerZoneAging -Aging $True -Verbose
+
+######################################################
+### Have not set up NTP service in the environment yet
+### Define Authoritative Internet Time Servers
+# $timePeerList = "0.us.pool.ntp.org 1.us.pool.ntp.org"
+
+### Set Time Configuration
+# w32tm /config /manualpeerlist:$timePeerList /syncfromflags:manual /reliable:yes /update
