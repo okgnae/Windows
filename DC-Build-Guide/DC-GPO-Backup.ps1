26a392a31b61597accb5ebbb216f14500c7c9c60
Reangko <33633448+reangko@users.noreply.github.com>
2020-05-19 14:35:40 +0300

    Update DC-GPO-Backup.ps1

--- DC-Build-Guide/DC-GPO-Backup.ps1
+++ DC-Build-Guide/DC-GPO-Backup.ps1
@@ -1,3 +1,22 @@
 ### DC-GPO-Backup
+$GpoBackupPath = "C:\Users\Administrator\Desktop\"
+$GpoBackupDir = "GpoBackup"
+$GpoBackupPathDir = $GpoBackupPath + $GpoBackupDir
 
-Backup-Gpo -All -Path C:\Users\Administrator\Desktop\
+### Create Back up Directory
+if (!(Test-Path $GpoBackupPathDir))
+{
+    New-Item -ItemType Directory -Path $GpoBackupPath -Name $GpoBackupDir
+}
+
+### Remove old Backups
+Get-ChildItem $GpoBackupPathDir | Remove-Item -Recurse -Force
+
+### BackUP all GPOs
+Backup-GPO -All -Path $GpoBackupPathDir
+
+### BackUP all GPO Links
+$GpoInherits = @()
+$GpoInherits += (Get-ADDomain).DistinguishedName | Get-GPInheritance
+$GpoInherits += (Get-ADOrganizationalUnit -Filter * | Where-Object -Property LinkedGroupPolicyObjects -Like -Value cn=*).DistinguishedName | Get-GPInheritance
+$GpoInherits.GpoLinks | Export-Csv -NoTypeInformation -Path "$GpoBackupPathDir\GpoLinks.csv"
