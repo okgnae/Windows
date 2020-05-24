5a5ae735092e05a65f28b12a3590f01489e079f4
Reangko <33633448+reangko@users.noreply.github.com>
2020-05-24 13:29:20 +0300

    Update DC-Build-DnsSitesTime.ps1

--- DC-Build-Guide/DC-Build-DnsSitesTime.ps1
+++ DC-Build-Guide/DC-Build-DnsSitesTime.ps1
@@ -20,6 +20,11 @@ Register-DnsClient -Verbose
 Set-DnsServerScavenging –ScavengingState $True –ScavengingInterval 7:00:00:00 –ApplyOnAllZones -Verbose
 Get-DnsServerZone | Where-Object {$_.IsAutoCreated -eq $False -and $_.ZoneName -ne 'TrustAnchors'} | Set-DnsServerZoneAging -Aging $True -Verbose
 
+### Create DNS Records for Kafka
+Add-DnsServerResourceRecordA -Name "KAFKA0001" -ZoneName "hq.corp" -IPv4Address "10.0.0.101" -CreatePtr
+Add-DnsServerResourceRecordA -Name "KAFKA0002" -ZoneName "hq.corp" -IPv4Address "10.0.0.102" -CreatePtr
+Add-DnsServerResourceRecordA -Name "KAFKA0003" -ZoneName "hq.corp" -IPv4Address "10.0.0.103" -CreatePtr
+
 ######################################################
 ### Have not set up NTP service in the environment yet
 ### Define Authoritative Internet Time Servers
